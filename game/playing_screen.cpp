aef0bbd1f9ab3315bbed3416cca20c57af12fceb
JackDesBwa <jackdesbwa@gmail.com>
2014-08-02 22:12:41 +0200

    Change some speed constants

--- game/playing_screen.cpp
+++ game/playing_screen.cpp
@@ -30,9 +30,14 @@ void PlayingScreen::animate() {
 	ball_speed.animate();
 
 	// Compute ball speed and new position
-	float real_speed = HMI_DTMS / (500.0 + (1 - ball_speed) * 1000.0 - ball_local_speed) * HMI_WIDTH * 2;
+	float real_speed = HMI_DTMS / (600.0 + (1 - ball_speed) * 1000.0 - ball_local_speed) * HMI_WIDTH * 2;
+	if (real_speed > HMI_DTMS / 500.0 * HMI_WIDTH * 2)
+		real_speed = HMI_DTMS / 500.0 * HMI_WIDTH * 2;
 	ball_position += real_speed * ball_direction;
-	ball.set_shiny(HMI_WIDTH - 20 < ball_position || 20 - HMI_WIDTH > ball_position);
+	ball.set_shiny(
+		(HMI_WIDTH - 20 < ball_position && ball_direction > 0) ||
+		(20 - HMI_WIDTH > ball_position && ball_direction < 0)
+	);
 	ball.set_position(ball_position);
 
 	// Check for win
@@ -52,7 +57,7 @@ void PlayingScreen::animate() {
 		if (20 - HMI_WIDTH > ball_position) {
 			pad1.fire(HMI_WIDTH + ball_position);
 			ball_direction = 1;
-			ball_local_speed = 15 * (20 - HMI_WIDTH - ball_position);
+			ball_local_speed = 50 * (20 - HMI_WIDTH - ball_position);
 		} else {
 			pad1.fire(20);
 		}
@@ -61,7 +66,7 @@ void PlayingScreen::animate() {
 		if (HMI_WIDTH - 20 < ball_position) {
 			pad2.fire(HMI_WIDTH - ball_position);
 			ball_direction = -1;
-			ball_local_speed = 15 * (20 - HMI_WIDTH + ball_position);
+			ball_local_speed = 50 * (20 - HMI_WIDTH + ball_position);
 		} else {
 			pad2.fire(20);
 		}
