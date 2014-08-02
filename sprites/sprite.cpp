1beeef0364a2d72fea2bfacacfdedef3330c246b
JackDesBwa <jackdesbwa@gmail.com>
2014-08-02 14:26:10 +0200

    Add Sprite class

---
+++ sprites/sprite.cpp
@@ -0,0 +1,47 @@
+#include "sprite.h"
+#include "../hmi/hmi_manager.h"
+
+Sprite::Sprite() : linked_to(0), reversed(false), position(0) {
+}
+
+void Sprite::init() {
+	linked_to = 0;
+	reversed = false;
+	position = 0;
+	init_sprite();
+}
+
+void Sprite::link_to(Sprite * l) {
+	linked_to = l;
+}
+
+void Sprite::reverse(bool active) {
+	if (reversed ^ active)
+		position = -position;
+	reversed = active;
+}
+
+void Sprite::set_position(float pos) {
+	if (reversed) pos = -pos;
+	position = pos;
+}
+
+float Sprite::get_position() {
+	return position;
+}
+
+float Sprite::get_abs_position() {
+	if (linked_to)
+		return linked_to->get_abs_position() + position;
+	else
+		return position;
+}
+
+bool Sprite::get_reversed() {
+	return reversed;
+}
+
+void Sprite::draw(int led, unsigned char r, unsigned char g, unsigned char b) {
+	if (reversed) led = -led;
+	hmi.leds.set(get_abs_position() + led, r, g, b);
+}
---
+++ sprites/sprite.h
@@ -0,0 +1,63 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+
+class Sprite {
+	public:
+		/** Simple constructor **/
+		Sprite();
+
+		/** Initialize sprite **/
+		void init();
+
+		/** Link to another sprite
+		  * @param [in] l Sprite to link to
+		  */
+		void link_to(Sprite * linked_to);
+
+		/** Revese coordinate system
+		  * @param [in] active Reverse (true) or not (false)
+		  */
+		void reverse(bool active);
+
+		/** Set position (in linked_to if any)
+		  * @param [in] pos New position
+		  */
+		void set_position(float pos);
+
+		/** Get position (in linked_to if any)
+		  * @return Position
+		  */
+		float get_position();
+
+		/** Get absolute position
+		  * @return Position
+		  */
+		float get_abs_position();
+
+		/** Get reverse property
+		  * @return Reverse property
+		  */
+		bool get_reversed();
+
+		/** Function to call for animation **/
+		virtual void animate() = 0;
+
+	protected:
+		/* Draw led in relative coordinates
+		  * @param [in] led Led number (relative)
+		  * @param [in] r   Red intensity   [0-255]
+		  * @param [in] g   Green intensity [0-255]
+		  * @param [in] b   Blue intensity  [0-255]
+		  */
+		void draw(int led, unsigned char r, unsigned char g, unsigned char b);
+
+	private:
+		/** Function to call for initialization **/
+		virtual void init_sprite() {}
+
+		Sprite * linked_to; // Pointer to sprite it is linked to
+		bool reversed; // Is coordinate system reversed ?
+		float position; // Current position
+};
+
+#endif
